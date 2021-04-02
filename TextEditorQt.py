364b8fa0e72f74ad58f2b8e7bf54df45872887a4
Farodin259 <36227428+Farodin259@users.noreply.github.com>
2021-04-03 02:49:21 +0300

    MateWriter (build 0.0.0.1)

--- TextEditorQt.py
+++ TextEditorQt.py
@@ -1,44 +1,80 @@
-#pyside6-uic TextEditor.ui -o TextEditorUI.py
-
+# pyside6-uic TextEditor.ui -o TextEditorUI.py
 from TextEditorUI import *
 from PySide6.QtWidgets import QApplication
 from TextEditorUI import Ui_MainWindow, QFont, QMainWindow, QAction  # импорт нашего сгенерированного файла
 from PySide6.QtCore import QSettings, QPoint, QSize
 import sys
 
+file_path = None
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
-
         self.setupUi(self)
         self.settings = QSettings('My company', 'myApp')
 
 
+        def closeEvent(self, e):
+            if not self.textEdit.document().isModified():
+                return
+            answer = QMessageBox.question(
+                window, None,
+                "You have unsaved changes. Save before closing?",
+                QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel
+            )
+            if answer & QMessageBox.Save:
+                from Tools.pynche.pyColorChooser import save
+                save()
+            elif answer & QMessageBox.Cancel:
+                e.ignore()
 
 
-        def open_file():
-            fname = QFileDialog.getOpenFileName(self, 'Open file')[0]
 
-            f = open(fname, 'r')
+        # TODO сохранять в файл который созданимм в new_file
+
+        def new_file():
+            f = open('Новый файл.txt', 'x')
+
+
             with f:
                 data = f.read()
                 self.textEdit.setText(data)
 
 
+        def open_file():
+            global file_path
+            path = QFileDialog.getOpenFileName(window, "Open")[0]
+            if path:
+                self.textEdit.setPlainText(open(path).read())
+                file_path = path
 
         def file_save():
-            name = QFileDialog.getSaveFileName(self, 'Save File')[0]
-            file = open(name, 'w')
-            text = self.textEdit.toPlainText()
-            file.write(text)
-            file.close()
+            if file_path is None:
+                save_as()
+            else:
+                with open(file_path, "w") as f:
+                    f.write(self.textEdit.toPlainText())
+
+                self.textEdit.document().setModified(False)
+
+
+
+        def save_as():
+            global file_path
+            path = QFileDialog.getSaveFileName(window, "Save As")[0]
+            if path:
+                file_path = path
+                file_save()
+
+
 
 
 
         self.exit_action.triggered.connect(QApplication.quit)
         self.save_action.triggered.connect(file_save)
         self.open_action.triggered.connect(open_file)
+        self.newfile_action.triggered.connect(new_file)
+        self.saveas_action.triggered.connect(save_as)
         self.open_action.setShortcut('Ctrl+O')
         self.newfile_action.setShortcut('Ctrl+N')
         self.save_action.setShortcut('Ctrl+S')
@@ -54,10 +90,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if windowScreenState:
             self.restoreState(windowScreenState)
 
-
-
-
-
     def closeEvent(self, e):
         # Write window size and position to config file
         self.settings.setValue("windowScreenGeometry", self.saveGeometry())
@@ -65,9 +97,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         e.accept()
 
 
-
 if __name__ == "__main__":
-
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
